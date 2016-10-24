69cba7b232ef64c76400d7044588970a5f9ef7ed
Lab3CerebusHost <miller.limblab@gmail.com>
2016-10-24 17:16:53 -0500

    updated psychophysics script

--- cerestimM96/psychophysicsStim.m
+++ cerestimM96/psychophysicsStim.m
@@ -6,6 +6,10 @@
     nPulses=floor(freq/duration);
     nStim=4;
     chans=[1];
+%% configuration for cbmex
+    wordStim=hex2dec('60');
+    maxWait=500;
+    pollInterval=.01;
 %% set up stimulation:
     stimObj=cerestim96;
     %connect to cerestim96
@@ -19,4 +23,33 @@
         stimObj.setStimPattern(i,0,nPulses,amp,amp,pWidth,pWidth,53,freq)
     end
 %% use cbmex to loop-wait for stim word. 
-%When stim word happens call cerestim96 to start pulse train:   
+%When stim word happens call cerestim96 to start pulse train:  
+
+while 1
+    eTime=tic;
+    while toc(eTime)<maxwait
+        
+        stimCodes=[];
+        try
+            words = read_words(); % read some data
+        catch % maybe cbmex wasn't initialized yet
+            CBInitWordRead(mode);
+            words = read_words(); % read some data
+        end
+        if ~isempty(words)
+            %scan for stim word:
+            stimCodes=words( bitand(hex2dec('f0'),words(:,2)) == wordStim,2);
+        end
+        if ~isempty(stimCodes)
+            if numel(stimcodes)>1
+                warning('psychophysicsStim:multipleStimWords','expected a single stim word, and got more than one. Continuing using only the first stim word')
+            end
+            %issue the stimulus:
+            stimObj.manualStim(chans,uint8(stimCodes))
+            break
+        end
+    end
+    pause(.00063+duration+.00053)
+end
+
+
